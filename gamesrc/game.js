460560e43c7c51c75aa6bade5117a89088d1dc95
Cuckson <31227094+Cuckson@users.noreply.github.com>
2022-02-23 14:45:24 +0000

    test

--- gamesrc/game.js
+++ gamesrc/game.js
@@ -317,6 +317,10 @@ class Game {
 		artistText.y = 300;
 		artistText.textAlign = "center";
 		this.mainMenuGfx.addChild(artistText);
+		// cd
+		var image = new Image();
+		image.src = "data/cd.png";
+		this.mainMenuGfx.addChild(image);
 		// add chart artist text
 		if (this.chartConfig.chartArtist) {
 			var chartistText = 'Chart by ' + this.chartConfig.chartArtist;
